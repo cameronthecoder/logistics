31be7442107c9d43ae13a50c4f29705cef23c224
Cameron Dahl <camnooten@gmail.com>
2022-07-06 17:42:56 -0500

    Monitor docker engine events with libsoup

--- src/docker.py
+++ src/docker.py
@@ -1,21 +1,49 @@
-import gi, json, logging
+import gi, json, logging, math
 
 gi.require_version('Soup', '3.0')
 gi.require_version('Adw', '1')
 from gi.repository import GObject, Adw, Soup, Gio, GLib, Gtk
 
+class ContainerConfig(GObject.GObject):
+    __gtype_name__ = 'ContainerConfig'
+    hostname = GObject.Property(type=str)
+    domainname = GObject.Property(type=str)
+    user = GObject.Property(type=str)
+    attachStdin = GObject.Property(type=bool, default=False)
+    tty = GObject.Property(type=bool, default=False)
+    env: Gtk.ListStore = GObject.Property(type=Gtk.ListStore)
+
+    def __init__(self, inspect_data, **kwargs):
+        super().__init__(**kwargs)
+        self.set_property("hostname", inspect_data["Hostname"])
+        self.set_property("domainname", inspect_data["Domainname"])
+        self.set_property("user", inspect_data['User'])
+        self.set_property("attachStdin", inspect_data['AttachStdin'])
+        self.set_property("tty", inspect_data['Tty'])
+        self.set_property("env", Gtk.ListStore(str))
+
+        [self.env.append([variable]) for variable in inspect_data['Env']]
+
 
 class Image(GObject.GObject):
     __gtype_name__ = 'Image'
+    id = GObject.Property(type=str)
     name = GObject.Property(type=str)
     version = GObject.Property(type=str)
+    size = GObject.Property(type=int)
+    container_config = GObject.Property(type=ContainerConfig)
+
 
     def __init__(self, image_data, **kwargs):
         super().__init__(**kwargs)
         name, version = image_data['RepoTags'][0].split(':')
-        print(name, version)
         self.set_property("name", name)
+        self.set_property("id", image_data['Id'])
         self.set_property("version", version)
+        self.set_property("size", image_data['Size'])
+
+    def add_inspection_info(self, inspect_data):
+        self.set_property("container_config", ContainerConfig(inspect_data['ContainerConfig']))
 
 
 @Gtk.Template(resource_path='/com/camerondahl/Logistics/ui/image_row.ui')
@@ -23,14 +51,22 @@ class ImageRow(Adw.ActionRow):
     __gtype_name__ = 'ImageRow'
 
     size = Gtk.Template.Child()
-    version = Gtk.Template.Child()
+
+    def convert_size(self, size_bytes):
+       if size_bytes == 0:
+           return "0B"
+       size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+       i = int(math.floor(math.log(size_bytes, 1024)))
+       p = math.pow(1024, i)
+       s = round(size_bytes / p, 2)
+       return "%s %s" % (s, size_name[i])
+
 
     def __init__(self, image, **kwargs):
         super().__init__(**kwargs)
-        print('image row created')
-        print(image.version)
+        self.image = image
         self.set_title(image.name)
-        self.version.set_label(image.version)
+        self.size.set_label(self.convert_size(image.size))
 
     def get_label(self):
         return self.get_title()
@@ -41,18 +77,41 @@ class DockerClient(GObject.Object):
         __gtype_name__ = 'dockerClient'
         object = GObject.property(type=GObject.Object)
 
-        def __init__(self):
+        def __init__(self, spinner):
             GObject.Object.__init__(self)
+            self.spinner: Gtk.Spinner = spinner
+            self.cancellable = Gio.Cancellable().new()
             self.session = Soup.Session()
+            self.session.set_timeout(0)
+            self.session.set_idle_timeout(0)
+            self.monitor_events()
 
 
-        def get_images(self, callback):
-            def on_images_response(session, result, message):
-                resp = session.send_and_read_finish(result)
+        def monitor_events(self):
+            def on_response(source: Soup.Session, res: Gio.Task, data: Soup.Message):
+                input_stream: Gio.InputStream = source.send_finish(res)
+
+                def on_callback(dataInputStream, res, user_data):
+                    lineout = dataInputStream.read_line_finish(res)
+                    print(lineout)
+                    data_input_stream.read_line_async(GLib.PRIORITY_DEFAULT, self.cancellable, on_callback, None)
+
+                data_input_stream = Gio.DataInputStream.new(input_stream)
+                data_input_stream.read_line_async(GLib.PRIORITY_DEFAULT, self.cancellable, on_callback, None)
+            message = Soup.Message.new("GET", "http://127.0.0.1:5555/events")
+            resp = self.session.send_async(message, GLib.PRIORITY_DEFAULT, self.cancellable, on_response, message)
+            print("waiting for call")
+            return message, resp
+
+
+
+        def make_api_call(self, url, callback):
+            def on_response(session, result, message):
                 data = None
                 success = None
                 error = None
                 try:
+                    resp = session.send_and_read_finish(result)
                     data = json.loads(resp.get_data())
                     success = True
                 except Exception as e:
@@ -60,8 +119,19 @@ class DockerClient(GObject.Object):
                     error = e
                 callback(success, error, data)
 
-
             self.cancellable = Gio.Cancellable().new()
-            msg = Soup.Message.new("GET", "http://127.0.0.1:5555/images/json")
-            resp = self.session.send_and_read_async(msg, GLib.PRIORITY_DEFAULT, self.cancellable, on_images_response, msg)
+            msg = Soup.Message.new("GET", url)
+            resp = self.session.send_and_read_async(msg, GLib.PRIORITY_DEFAULT, self.cancellable, on_response, msg)
+            return msg, resp
 
+        def get_images(self, callback):
+            self.spinner.start()
+            self.make_api_call("http://127.0.0.1:5555/images/json", callback)
+
+        def get_containers(self, callback):
+            self.make_api_call("http://127.0.0.1:5555/containers/json?all=true", callback)
+
+        def inspect_image(self, name, callback):
+            self.make_api_call(f"http://127.0.0.1:5555/images/{name}/json",callback)
+
+            
--- src/window.py
+++ src/window.py
@@ -17,7 +17,7 @@
 
 #from .cp import ContainersPage
 
-from gi.repository import Adw, Gtk, Gio, GObject
+from gi.repository import Adw, Gtk, Gio, GObject, GLib
 from .docker import DockerClient, Image, ImageRow
 
 
@@ -29,26 +29,87 @@ class ContainersPage(Adw.Bin):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+@Gtk.Template(resource_path='/com/camerondahl/Logistics/ui/image_dialog.ui')
+class ImageDialog(Adw.Window):
+    __gtype_name__ = 'ImageDialog'
+
+    spinner = Gtk.Template.Child()
+    status_page = Gtk.Template.Child()
+
+    container_group = Gtk.Template.Child()
+    hostname_row = Gtk.Template.Child()
+    hostname_label = Gtk.Template.Child()
+
+    def __init__(self, image, window, **kwargs):
+        super().__init__(**kwargs)
+        self.set_title(image.name)
+        self.set_transient_for(window)
+        self.window = window
+        self.image = image
+        self.window.spinner.start()
+        self.window.client.inspect_image(image.name, self.on_inspect_callback)
+
+    def on_inspect_callback(self, success, error, data):
+        if data:
+            print(data)
+            self.spinner.stop()
+            self.hostname_row.set_title("Test")
+            self.image.add_inspection_info(data)
+            self.status_page.set_visible(False)
+
+            self.hostname_label.set_label(self.image.container_config.hostname)
+        self.window.spinner.stop()
+
+
+
 @Gtk.Template(resource_path='/com/camerondahl/Logistics/ui/images_page.ui')
 class ImagesPage(Adw.Bin):
     __gtype_name__ = 'ImagesPage'
 
     images_list = Gtk.Template.Child()
     label = Gtk.Template.Child()
+    status_page = Gtk.Template.Child()
 
     store = Gio.ListStore.new(Image)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         print(self.images_list)
+        self.images_list.set_selection_mode(Gtk.SelectionMode.SINGLE)
         self.images_list.bind_model(self.store, lambda f: ImageRow(f))
-        self.client = DockerClient()
-        self.client.get_images(self.on_images_response)
+        self.images_list.connect("row-selected", self.row_selected)
+
+
+    def row_selected(self, row, container):
+        dialog = ImageDialog(container.image, self.window)
+        dialog.set_modal(True)
+        dialog.present()
+
+    def set_window(self, window):
+        print(window)
+        self.window = window
+        self.window.client.get_images(self.on_images_response)
+
+
+    def get_images(self):
+        print("GET IMAGES CALL")
+        self.store.remove_all() # TODO: Use docker engine
+        self.window.client.get_images(self.on_images_response)
+        return True
+
 
     def on_images_response(self, success, error, data):
-        if data:
-            for image in data:
-                self.store.append(Image(image))
+        print("IMAGES RESPONSE")
+        if error:
+            self.label.set_visible(False)
+            self.images_list.set_visible(False)
+            self.status_page.set_visible(True)
+        if data and success:
+            self.label.set_visible(True)
+            self.images_list.set_visible(True)
+            self.status_page.set_visible(False)
+            [self.store.append(Image(image)) for image in data]
+        self.window.spinner.stop()
 
 @Gtk.Template(resource_path='/com/camerondahl/Logistics/ui/window.ui')
 class LogisticsWindow(Adw.ApplicationWindow):
@@ -58,9 +119,13 @@ class LogisticsWindow(Adw.ApplicationWindow):
     view_stack = Gtk.Template.Child()
     images_page = Gtk.Template.Child()
     containers_page = Gtk.Template.Child()
+    spinner = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.app = kwargs['application']
+        self.client = DockerClient(self.spinner)
+        self.images_page.set_window(self)
 
 
 
